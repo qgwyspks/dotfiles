ae53d83a0ffde86df2af456f44765fb87755a64d
qgwyspks <1278649519@qq.com>
2023-09-11 19:27:42 +0800

    Add wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm'
+-- local keybinds = require 'keybinds'
+
+local config = {
+  font = wezterm.font("JetBrains Mono"),
+  font_size = 10,
+
+  window_background_opacity = 0.80
+  
+
+}
+
+return config
